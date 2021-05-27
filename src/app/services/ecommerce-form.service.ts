5d9a988d5062a45773e59c4eec35b835492011b7
BrunoliveiraG <brunoliveiragomesz@gmail.com>
2021-05-27 14:00:59 -0300

    commit do projeto

---
+++ src/app/services/ecommerce-form.service.ts
@@ -0,0 +1,45 @@
+import { Observable, of } from 'rxjs';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EcommerceFormService {
+
+  constructor() { }
+
+
+  getCreditCardMonths(startMonth: number): Observable<number[]> {
+
+    let data: number[] = [];
+
+    // build an array for "Month" dropdown list
+    // -start at current month and loop until
+  
+    for(let theMonth = startMonth; theMonth <= 12; theMonth++){
+      data.push(theMonth);
+    }
+    
+    // "of" operator from rxjs will wrap an object as an Observable
+    return of(data);
+  }
+
+  getCreditCardYears(): Observable<number[]> {
+    
+    let data: number[] = [];
+
+    // build an array for "Year" downlist list
+    // -start at current year and lopp for next 10 years
+
+    const startYear: number = new Date().getFullYear();
+    const endYear: number = startYear + 10;
+
+    for (let theYear = startYear; theYear <= endYear; theYear++){
+
+      data.push(theYear);
+    }
+
+    return of(data);
+  }
+
+}
